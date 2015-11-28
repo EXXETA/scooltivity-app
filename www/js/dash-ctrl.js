22b63611b179a4cb3835073f24569c9092f5505a
Eugen Melechow <eugen.melechow@exxeta.de>
2015-11-28 13:36:31 +0100

    Removed obsolete files

--- www/js/dash-ctrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('starter.controllers.dash', [])
-
-.controller('DashCtrl', function($scope, $ionicPopup, $state) {
-  
-  $scope.showAddTab = 'TEACHER' === window.localStorage['role'];
-  
-  
-});
